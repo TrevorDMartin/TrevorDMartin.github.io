b332abc289ee3c5833963572c750beb48bdc986c
Trevor Martin <tdm179012@gmail.com>
2025-12-27 11:24:23 -0500

    clean up About

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -5,19 +5,12 @@
 <BodySection heading="About Cassettiquette">
   <div class="bio">
     <p>
-      Cassettiquette is an indie rock band known for nostalgic soundscapes and introspective lyrics.
-    </p>
-    <p>
-      Drawing inspiration from the analog warmth of cassette tapes, they blend raw emotion with
-      polished sensibilities.
-    </p>
-    <p>
       Cassettiquette is an indie rock band from Charlotte, NC, blending introspective lyrics with
-      dreamy, atmospheric sounds. Fronted by Connor Hausman (vocals/guitar), with Emma Freas
+      dreamy, atmospheric sounds.<br/>Fronted by Connor Hausman (vocals/guitar), with Emma Freas
       (backing vox/keys) and Trevor Martin (drums) rounding out the core, the band's music explores
       a mix of personal and fictional stories. Known for their diverse thematic range,
       Cassettiquette crafts songs that invite listeners into a world of reflection, emotion, and
-      narrative depth. In 2024, the band was recognized as Charlotte's Best New Band by Queen City
+      narrative depth.<br/>In 2024, the band was recognized as Charlotte's Best New Band by Queen City
       Nerve. Their sound continues to evolve while staying rooted in evocative storytelling.
     </p>
   </div>
@@ -27,6 +20,9 @@
   .bio {
     line-height: 1.8;
     font-size: 1.1rem;
+    max-height: 300px;
+    overflow-y: auto;
+    padding-right: 1rem;
   }
   p {
     margin-bottom: 1rem;
